1e4e37f5337f62ea4ac78f81852b9659c79f1c2e
malokasheko <malokasheko@gmail.com>
2024-12-04 00:39:14 +0200

    finish

--- src/connecthub/ProfileManagement.java
+++ src/connecthub/ProfileManagement.java
@@ -8,6 +8,55 @@ package connecthub;
  *
  * @author malok
  */
+   
 public class ProfileManagement {
+    private String username;
+    private String hashpass;
+    private String bio;
+    private String profilePhoto;
+    private String coverPhoto;
+     private ProfileManagement(profileBuilder builder) {
+            this.username =builder.username;
+            this.hashpass = builder.hashpass;
+            this.bio = builder.bio;
+            this.profilePhoto = builder.profilePhoto;
+            this.coverPhoto = builder.coverPhoto;
+        }
     
+ public static class profileBuilder{
+    private String username;
+    private String hashpass;
+    private String bio;
+    private String profilePhoto;
+    private String coverPhoto;
+   
+    public profileBuilder Username(String username){
+    this.username=username;
+    return this;
+    }
+     public profileBuilder Hashpass(String hashpass){
+    this.hashpass=hashpass;
+    return this;
+    }
+      public profileBuilder Bio(String bio){
+    this.bio=bio;
+    return this;
+    }
+       public profileBuilder ProfilePhoto(String profilePhoto){
+    this.profilePhoto=profilePhoto;
+    return this;
+    }
+        public profileBuilder CoverPhoto(String coverPhoto){
+    this.coverPhoto=coverPhoto;
+    return this;
+    }
+        public  profileBuilder build(){
+        
+            return this;
+        }
+        
+        
+ }
+
+
 }
