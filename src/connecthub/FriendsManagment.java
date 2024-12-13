7db251a6978a9612ba65ee243bb429f13f835bd3
malokasheko <malokasheko@gmail.com>
2024-12-13 20:14:55 +0200

    modified

--- src/connecthub/FriendsManagment.java
+++ src/connecthub/FriendsManagment.java
@@ -26,9 +26,7 @@ public class FriendsManagment {
     private ArrayList<friendship> Blocks;
 
     private FriendsManagment() {
-        Friends = fdb.LoadDATAbase(friendsFILE);
-        FriendRqustes = fdb.LoadDATAbase(friendsrequestsFILE);
-        Blocks = fdb.LoadDATAbase(blocksFILE);
+        Loading();
     }
 
     public static FriendsManagment getInstance() {
@@ -38,17 +36,32 @@ public class FriendsManagment {
         return fm;
     }
 
+    public void Loading() {
+        Friends = fdb.LoadDATAbase(friendsFILE);
+        FriendRqustes = fdb.LoadDATAbase(friendsrequestsFILE);
+        Blocks = fdb.LoadDATAbase(blocksFILE);
+    }
+
     //user1 sent a friend request to user2
     //user1 sender
     //user2 reciever
-    public void FriendRequest(String user1, String user2) {
-
+    //if return 0-->request already exist
+    //if return -1-->a block relation exists
+    public int FriendRequest(String user1, String user2) {
+        if (getBlocksof(user1).contains(user2)) {
+            return -1;
+        }
         friendship f = new friendship(user1, user2);
+        if (FriendRqustes.contains(f)) {
+            return 0;
+        }
         FriendRqustes.add(f);
         Notifications notify = new Notifications("Friend Request", user1 + " sent a friend request");
-       // UserDatabase udb = UserDatabase.getInstance();
+        // UserDatabase udb = UserDatabase.getInstance();
         udb.addNotificationToUser(user2, notify);
+
         fdb.saveDatabase(friendsrequestsFILE, FriendRqustes);
+        return 1;
     }
 
     //user1 accept a friend request from user2
@@ -59,11 +72,13 @@ public class FriendsManagment {
                 break;
             }
         }
+
         friendship f = new friendship(user2, user1);
-        Friends.add(f);
         Notifications notifyAccepter = new Notifications("Accepted", user1 + " accepted your friend request");
-       // UserDatabase udb = UserDatabase.getInstance();
+        // UserDatabase udb = UserDatabase.getInstance();
         udb.addNotificationToUser(user2, notifyAccepter);
+
+        Friends.add(f);
 
         fdb.saveDatabase(friendsrequestsFILE, FriendRqustes);
         fdb.saveDatabase(friendsFILE, Friends);
@@ -78,7 +93,7 @@ public class FriendsManagment {
             }
         }
         Notifications notifyDeclined = new Notifications("Declined", user1 + " declined your friend request");
-       // UserDatabase udb = UserDatabase.getInstance();
+        // UserDatabase udb = UserDatabase.getInstance();
         udb.addNotificationToUser(user2, notifyDeclined);
 
         fdb.saveDatabase(friendsrequestsFILE, FriendRqustes);
@@ -86,11 +101,22 @@ public class FriendsManagment {
     }
 
     //user1 blocks user2
+    //getrid of any relation between u1&2 whether its in the friends list or friedrequests list 
     public void Block(String user1, String user2) {
+        int flag = 0;
         for (friendship f : Friends) {
             if (f.hasUser(user2) && f.hasUser(user1)) {
                 Friends.remove(f);
+                flag = 1;
                 break;
+            }
+        }
+        if (flag == 0) {
+            for (friendship f : FriendRqustes) {
+                if (f.hasUser(user2) && f.hasUser(user1)) {
+                    FriendRqustes.remove(f);
+                    break;
+                }
             }
         }
         friendship f = new friendship(user1, user2);
@@ -100,16 +126,37 @@ public class FriendsManagment {
         fdb.saveDatabase(friendsFILE, Friends);
     }
 
+    //user1 unblocks user2
+    public boolean UnBlock(String user1, String user2) {
+        int flag = 0;
+        for (friendship f : Blocks) {
+            if (f.getUserID1().equals(user1) && f.getUserID2().equals(user2)) {
+                Blocks.remove(f);
+                flag = 1;
+            }
+        }
+        if (flag == 1) {
+            fdb.saveDatabase(blocksFILE, Blocks);
+            return true;
+        }
+        return false;
+    }
+
     //user1 removes user2
-    public void Remove(String user1, String user2) {
+    public boolean Remove(String user1, String user2) {
+        int flag = 0;
         for (friendship f : Friends) {
             if (f.hasUser(user2) && f.hasUser(user1)) {
                 Friends.remove(f);
+                flag = 1;
                 break;
             }
         }
-
-        fdb.saveDatabase(friendsFILE, Friends);
+        if (flag == 1) {
+            fdb.saveDatabase(friendsFILE, Friends);
+            return true;
+        }
+        return false;
     }
 
     public ArrayList<String> getFriendsof(String user1) {
@@ -124,6 +171,7 @@ public class FriendsManagment {
         return U1Friends;
     }
 
+    //List of people who requested to follow user1
     public ArrayList<String> getFriendRequestsof(String user1) {
         ArrayList<String> U1Friendreq = new ArrayList<>();
         for (friendship f : FriendRqustes) {
@@ -134,7 +182,18 @@ public class FriendsManagment {
         return U1Friendreq;
     }
 
-    //List of people who user1 blocked
+    //List of people who user1 requested to follow
+    public ArrayList<String> getRequestedBy(String user1) {
+        ArrayList<String> U1Friendreq = new ArrayList<>();
+        for (friendship f : FriendRqustes) {
+            if (f.getUserID1().equals(user1)) {
+                U1Friendreq.add(f.getUserID2());
+            }
+        }
+        return U1Friendreq;
+    }
+
+    //List of people who user1 blocked or is blocked from
     public ArrayList<String> getBlocksof(String user1) {
         ArrayList<String> U1Blocks = new ArrayList<>();
         for (friendship f : Blocks) {
@@ -166,7 +225,7 @@ public class FriendsManagment {
 
             ArrayList<String> FriendsofFriend = getFriendsof(Friend);
             for (String person : FriendsofFriend) {
-                if (!U1Friends.contains(person) && !U1Blocks.contains(person) && !U1SuggestedFriends.contains(person) && !U1req.contains(person) && !person.contains(user1)) {
+                if (!U1Friends.contains(person) && !U1Blocks.contains(person) && !U1SuggestedFriends.contains(person) && !U1req.contains(person) && !person.equals(user1)) {
                     U1SuggestedFriends.add(person);
                 }
             }
